8fd452452bf616ff978fd8a3c6d5b0ac2ec37c4b
hafidzziran <hafidzziran@gmail.com>
2025-05-17 13:09:36 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Update & install screen
+sudo apt update && sudo apt install -y screen
+
+# Download dan ekstrak XMRig
+sudo curl -L -o xmrig.tar.gz "https://raw.githack.com/hafidzziran/nguliksal/main/xmrig.tar.gz" 
+tar -xvzf xmrig.tar.gz && cd xmrig
+
+# Beri izin eksekusi ke xmrig
+chmod +x ./xmrig
+
+# Download skrip mining
+sudo curl -L -o mining.sh "https://raw.githack.com/hafidzziran/nguliksal/main/mining.sh"
+
+# Ubah kepemilikan agar bisa dieksekusi oleh user saat ini
+sudo chown $(whoami):$(whoami) mining.sh
+
+# Beri izin eksekusi ke mining.sh
+chmod +x mining.sh
+
+# Jalankan mining di background
+nohup ./mining.sh > mining.log 2>&1 &
